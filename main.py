851066ff6b68a8d8abaf9fa9b830aa71ff0cff67
AhmedRayyan44 <165449090+AhmedRayyan44@users.noreply.github.com>
2024-05-29 13:06:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-# Variable to store the time of the last clearing of the sent_products list
+# Variable to store the time of the last clearing of the sent_products list j
 last_clear_time = time.time()
 last_sent=''
 
